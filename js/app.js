046024cec2f739840a3408b24ccb6c0108bf1a8f
manojadhikari22 <manojadhikari2207@gmail.com>
2023-11-07 10:15:02 +0100

    List of different website and their links

---
+++ js/app.js
@@ -0,0 +1,21 @@
+let header = document.querySelector('header');
+
+let currentScrollpos = 0; //Initializing variable to 0.
+let scrollDirection = 'down'; // Initializing variable to down.
+
+const activeheader = () => {
+    const prevScrollpos = window.scrollY;
+    if(prevScrollpos > currentScrollpos) {
+        scrollDirection = 'down';
+    } else {
+        scrollDirection = 'up';
+    }
+    if( scrollDirection === 'down' && prevScrollpos > 500) {
+        header.classList.add('header-hidden');
+    }
+    if(scrollDirection === 'up') {
+        header.classList.remove('header-hidden');
+    }
+    currentScrollpos = prevScrollpos;
+}
+window.addEventListener('scroll', activeheader);// eventListener foe the scroll event on the 'window' object.
